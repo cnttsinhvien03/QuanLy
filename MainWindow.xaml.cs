ff001ea896c19ac4bf88f3ff5ef0b1e2c2045cbc
Admin <Admin@DESKTOP-7QHBA3R>
2025-03-28 07:32:21 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QuanLyBanHang.ViewModels;
+
+
+namespace QuanLyBanHang
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+        private void ToggleProductMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductSubMenu.Visibility == Visibility.Visible)
+            {
+                ProductSubMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ProductSubMenu.Visibility = Visibility.Visible;
+            }
+        }
+        private void ToggleImportMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (ImportSubMenu.Visibility == Visibility.Visible)
+            {
+                ImportSubMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ImportSubMenu.Visibility = Visibility.Visible;
+            }
+        }
+        private void ToggleSellMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (SellSubMenu.Visibility == Visibility.Visible)
+            {
+                SellSubMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                SellSubMenu.Visibility = Visibility.Visible;
+            }
+        }
+        private void ToggleWarehouseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (WarehouseSubMenu.Visibility == Visibility.Visible)
+            {
+                WarehouseSubMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                WarehouseSubMenu.Visibility = Visibility.Visible;
+            }
+        }
+        private void ToggleFundMenu_Click(object sender, RoutedEventArgs e)
+        {
+            FundSubMenu.Visibility = FundSubMenu.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+        /**private void ToggleDebtMenu_Click(object sender, RoutedEventArgs e)
+        {
+            DebtSubMenu.Visibility = DebtSubMenu.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+        private void ToggleWarrantyMenu_Click(object sender, RoutedEventArgs e)
+        {
+            WarrantySubMenu.Visibility = WarrantySubMenu.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+        private void ToggleAssetMenu_Click(object sender, RoutedEventArgs e)
+        {
+            AssetSubMenu.Visibility = AssetSubMenu.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+        private void ToggleReportMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ReportSubMenu.Visibility = ReportSubMenu.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+        private void ToggleOrderMenu_Click(object sender, RoutedEventArgs e)
+        {
+            OrderSubMenu.Visibility = OrderSubMenu.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }**/
+
+    }
+}
---
+++ Views/UserManagementView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using QuanLyBanHang.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyBanHang.Views
+{
+    /// <summary>
+    /// Interaction logic for UserManagementView.xaml
+    /// </summary>
+    public partial class UserManagementView : UserControl
+    {
+        public UserManagementView()
+        {
+            InitializeComponent();
+            this.DataContext = new UserManagementViewModel();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void QuayLai_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this)?.Close();
+        }
+    }
+}
